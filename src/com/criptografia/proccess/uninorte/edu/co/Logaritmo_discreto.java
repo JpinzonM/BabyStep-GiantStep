d04249d9341f40e05892e5fcdf511af7296fac0e
JPinzonM <apinzonj@uninorte.edu.co>
2019-03-23 07:59:00 -0500

    - validate if y is a element of finite cicle group.

--- src/com/criptografia/proccess/uninorte/edu/co/Logaritmo_discreto.java
+++ src/com/criptografia/proccess/uninorte/edu/co/Logaritmo_discreto.java
@@ -5,85 +5,98 @@
  */
 package com.criptografia.proccess.uninorte.edu.co;
 
-
 import java.math.BigDecimal;
 import java.util.Scanner;
 import java.math.BigInteger;
 import java.util.ArrayList;
+
 /**
  *
  * @author Jorge
  */
 public class Logaritmo_discreto {
-    
-    
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.println("ingrese numero primo: \n");
-        BigInteger p, m, m2, g;
+        BigInteger p, m, y, g;
         ArrayList<Integer> Zp = new ArrayList<Integer>();
         boolean Generador;
         //m1 double value to obtain a sqrt
-        double m1; 
-        //m2 value of m1 convert on biginteger.
-        p = new BigInteger(sc.nextLine()); 
-        boolean prime; 
-        Zp = mostrarZp(p);
-        System.out.println("ingrese el valor del generador g: \n");
-        g = new BigInteger(sc.nextLine());
-        Generador = gesgenerador(g, p);
-        if (Generador) {
-            System.out.println("el valor de g: "+ g +" resulto " + Generador + ", entonces Si es un generador de Zp");
-            prime = p.isProbablePrime(200);
-            
-            if (prime) {
-                m = (p.subtract(BigInteger.ONE));
-                System.out.println("m: "+ m);
-                m1 = Math.sqrt(m.doubleValue());
-                System.out.println("sqrt de m: "+ m1);
+        double m1;
 
-            }else{
-                System.out.println("El numero "+ p +" ingresado no es primo.");
+        p = new BigInteger(sc.nextLine());
+        boolean prime;
+
+        prime = p.isProbablePrime(200);
+
+        if (prime) {
+            Zp = mostrarZp(p);
+            System.out.println("ingrese el valor del generador g: ");
+            g = new BigInteger(sc.nextLine());
+            Generador = gesgenerador(g, p);
+            if (Generador) {
+                System.out.println("el valor de g: " + g + " resulto " + Generador + ", entonces Si es un generador de Zp");
+                System.out.println("ingrese el elemento y: ");
+                y = new BigInteger(sc.nextLine());
+                while (!elemY(y, Zp)) {
+                    System.out.println("ingrese el elemento y valido: ");
+                    y = new BigInteger(sc.nextLine());
+                }
+                System.out.println("el elemento " + y + " pertenece a Zp");
+                m = (p.subtract(BigInteger.ONE));
+                System.out.println("m: " + m);
+                m1 = Math.sqrt(m.doubleValue());
+                System.out.println("sqrt de m: " + m1);
+
+            } else {
+                System.out.println("el valor de g: " + g + " resulto " + Generador + "entonces No es un generador de Zp");
             }
-        }else{
-            System.out.println("el valor de g: "+ g +" resulto " +  Generador +  "entonces No es un generador de Zp");
+        } else {
+            System.out.println("El numero " + p + " ingresado no es primo.");
         }
-            
-        
-        
-        
     }
 
-    /***
-     * mostrar el grupo fimito ciclico definido por p
+    /**
+     * *
+     * mostrar el grupo finito ciclico definido por p
+     *
      * @param p
-     * @return 
+     * @return
      */
     private static ArrayList<Integer> mostrarZp(BigInteger p) {
         ArrayList<Integer> Zp = new ArrayList<Integer>();
-        String Z = "Zp={"; 
-        
+        String Z = "Zp={";
+
         for (int i = 1; i <= p.byteValue() - 1; i++) {
             Zp.add(i);
         }
         for (int i = 0; i < Zp.size(); i++) {
-            Z = Z + Zp.get(i) +",";
+            Z = Z + Zp.get(i) + ",";
         }
-        Z = Z.substring(0, Z.length()-1);
-        Z = Z + "}"; 
+        Z = Z.substring(0, Z.length() - 1);
+        Z = Z + "}";
         System.out.println(Z);
         return Zp;
     }
 
+    /**
+     * *
+     * valida si g es un generador del grupo finito ciclico.
+     *
+     * @param g
+     * @param p
+     * @return
+     */
     private static boolean gesgenerador(BigInteger g, BigInteger p) {
         int n = p.intValue();
         int ng = g.intValue();
         int ip, ig;
-        boolean Generador = false; 
+        boolean Generador = false;
         ArrayList<Integer> divp = new ArrayList<Integer>();
         ArrayList<Integer> divg = new ArrayList<Integer>();
-        for (int i = 1; i <= n ; i++) {
+        for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 divp.add(i);
             }
@@ -93,19 +106,38 @@ public class Logaritmo_discreto {
                 divg.add(i);
             }
         }
-        
+
         for (int i = 0; i < divp.size(); i++) {
             ip = divp.get(i);
             for (int j = 0; j < divg.size(); j++) {
                 ig = divg.get(j);
                 if (ig == ip) {
-                    Generador = false;                    
-                }else{
-                    Generador = true; 
+                    Generador = false;
+                } else {
+                    Generador = true;
                 }
             }
         }
-        
-        return Generador; 
+
+        return Generador;
+    }
+
+    /**
+     * *
+     * valida si el elemento y ingresado se encuentra dentro del grupo finito
+     * ciclico.
+     *
+     * @param y
+     * @param Zp
+     * @return
+     */
+    private static boolean elemY(BigInteger y, ArrayList<Integer> Zp) {
+        boolean state = false;
+        for (int i = 0; i < Zp.size(); i++) {
+            if (y.intValue() == (Zp.get(i))) {
+                state = true;
+            }
+        }
+        return state;
     }
 }
